cce83e553dcf36e5bdcbe1e13c9b017e5fbd31a6
Adil Shamuratov <iswearican_1@outlook.com>
2023-03-04 20:16:41 +0600

    changed model

--- news/models.py
+++ news/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class News(models.Model):
     title = models.CharField(max_length=255, verbose_name='Наименование новости')
-    body = models.TextField(max_length=255, verbose_name='Описание новости')
+    body = models.TextField(verbose_name='Описание новости')
     image = models.ImageField(upload_to='news/%Y/%m/%d/', verbose_name='Изображение новости')
     created_at = models.DateTimeField(auto_now=True, verbose_name='Дата создания новости')
 
